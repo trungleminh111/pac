d602eb7a8fa9ebd84a0b0d076faaf4ee9bd94f3c
Trung le <trung.le@techsolutions.vn>
2026-06-04 16:20:57 +0700

    Resolve package-lock using dynamic-data1111111

--- src/app/[locale]/cong-trinh/[slug]/page.tsx
+++ src/app/[locale]/cong-trinh/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/site/Header";
+import { SiteHeader as Header } from "@/components/site/SiteHeader";
 import { Footer } from "@/components/site/Footer";
 import { PageHeader } from "@/components/site/PageHeader";
 
